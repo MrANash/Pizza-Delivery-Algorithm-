e2c326490935e0caf826d0d9fd8b688c31b6baab
Adam Nash <adam.nash@live.com>
2021-06-08 12:33:43 -0600

    payment methods

---
+++ PizzaAlgorithm.cs
@@ -0,0 +1,19 @@
+decide to eat dinner
+pick pizza as food option
+pizza choices:
+    cheese
+    pepperoni
+    supreme
+    hawaiian
+    custom
+order online 
+pay for pizza
+get notifications about preparing and delivering pizza
+wait 
+take pizza from driver
+eat
+
+    Checkout:
+        Use Cash
+        Use Credit
+        Use 3rd party system
